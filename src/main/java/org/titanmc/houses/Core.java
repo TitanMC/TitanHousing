e20f50f538170ee62a640877df5c3e53efefbffb
SubToCashton <ashton1heslop@gmail.com>
2019-12-23 15:39:22 +1000

    Starting Coding the plugin
    Starting on Utils than moving to Events and GUIs

---
+++ src/main/java/org/titanmc/houses/Core.java
@@ -0,0 +1,8 @@
+package org.titanmc.houses;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Core extends JavaPlugin {
+    private static Core instance;
+    public static String serverVersion;
+}
